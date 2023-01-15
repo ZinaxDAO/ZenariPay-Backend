6309cec3d5d57582eb8f3cae9e8f9f44c09acc20
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-01-15 16:51:24 +0100

    update

---
+++ app/Http/Controllers/PaymentMethodController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paymentmethod;
+use Illuminate\Http\Request;
+
+class PaymentMethodController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $getMethods = Paymentmethod::where('user_id', $request->user()->id)->get();
+            return get_success_response($getMethods);
+        } catch (\Throwable $th) {
+            return get_error_response($th->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            //validate data
+
+            $payment_info = [];
+            foreach ($request->post() as $key => $v) {
+                $payment_info[$key] = $v;
+            }
+            // create new payment method for user
+            $method = new Paymentmethod();
+            $method->user_id = $request->user()->id;
+            $method->method_name = $request->method_name;
+            $method->payment_info = $payment_info;
+            if ($method->save()) {
+                return get_success_response(['msg' => 'Payment method added successfull']);
+            }
+        } catch (\Throwable $th) {
+            return get_error_response($th->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+            //validate data
+
+            $method = Paymentmethod::whereId($id);
+            if ($method) {
+                return get_success_response($method);
+            }
+        } catch (\Throwable $th) {
+            return get_error_response($th->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            //validate data
+
+            $payment_info = [];
+            foreach ($request->post() as $key => $v) {
+                $payment_info[$key] = $v;
+            }
+            // create new payment method for user
+            $method = new Paymentmethod();
+            $method->user_id = $request->user()->id;
+            $method->method_name = $request->method_name;
+            $method->payment_info = $payment_info;
+            if ($method->save()) {
+                return get_success_response(['msg' => 'Payment method added successfull']);
+            }
+        } catch (\Throwable $th) {
+            return get_error_response($th->getMessage(), 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $method = Paymentmethod::whereId($id);
+            if ($method->delete()) {
+                return get_success_response(['msg' => 'Payment method deleted successfully']);
+            }
+        } catch (\Throwable $th) {
+            return get_error_response($th->getMessage(), 500);
+        }
+    }
+}

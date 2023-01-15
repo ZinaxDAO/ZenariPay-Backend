315799c280c5a81d2092edb59f5351123d87b5af
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-01-15 14:26:34 +0100

    update

--- app/Models/TradeHistory.php
+++ app/Models/TradeHistory.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class TradeHistory extends Model
 {
     use HasFactory;
+
+    public function payment_method(){
+        return $this->belongsTo(PaymentMethodData::class, 'payment_id', 'paymentMethod_id');
+    }
+
+    public function trade()
+    {
+        return $this->belongsTo(Trade::class);
+    }
 }

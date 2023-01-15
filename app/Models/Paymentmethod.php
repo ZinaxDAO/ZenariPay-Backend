3a7b34c77531433a233a53c3bfe3f7efe5f5645b
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-01-15 15:02:08 +0100

    update

--- app/Models/Paymentmethod.php
+++ app/Models/Paymentmethod.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Paymentmethod extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $casts = [
+        'payment_info'  =>  'array'
+    ];
 }

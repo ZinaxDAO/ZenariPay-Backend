c750ccb1a141f1a14281951660e099b291a8d8eb
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-02-03 01:26:29 +0100

    update

--- app/Models/Trade.php
+++ app/Models/Trade.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Trade extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    
+    public function payment_info()
+    {
+        return $this->belongsTo(Paymentmethod::class, 'paymentMethod');
+    }
 }
